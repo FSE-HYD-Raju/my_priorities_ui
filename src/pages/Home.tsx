87676b49f67734886d0aef631685a260476174ef
Sruthi Mandulla <qvantel@QINDM051.local>
2020-02-03 22:40:51 +0530

    first

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,23 +1,36 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonRow, IonCol, IonGrid } from '@ionic/react';
 import React from 'react';
 
 const Home: React.FC = () => {
+  const test = {
+    "border": "1px solid red"
+  }
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Ionic Blank</IonTitle>
+          <IonTitle>My Priorities</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent className="ion-padding">
-        The world is your oyster.
-        <p>
-          If you get lost, the{' '}
-          <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/">
-            docs
-          </a>{' '}
-          will be your guide.
-        </p>
+        <IonGrid>
+          <IonRow>
+            <IonCol class="home-col">
+              Do First
+          </IonCol>
+            <IonCol>
+              Do Later
+          </IonCol>
+          </IonRow>
+          <IonRow>
+            <IonCol>
+              Delegate
+          </IonCol>
+            <IonCol>
+              Eleminate
+          </IonCol>
+          </IonRow>
+        </IonGrid>
       </IonContent>
     </IonPage>
   );
